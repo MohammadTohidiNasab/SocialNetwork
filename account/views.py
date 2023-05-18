fc159a824422bde76e93a10b4a2cb8200105106f
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-05-18 12:00:35 +0330

    register form and template

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
