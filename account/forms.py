f59245e4f1e65b87a0483cf82ecdf31cd98ccc79
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-05-25 22:39:23 +0330

    UserRegistr view and form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .models import Profile
 class UserRegistrationForm(forms.Form):
 	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -20,3 +22,9 @@ class UserRegistrationForm(forms.Form):
 
 		if p1 and p2 and p1 != p2:
 			raise ValidationError('password must match')
+
+
+
+class UserLoginForm(forms.Form):
+	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- account/views.py
+++ account/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm, EditUserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from .models import Relation
+
 # Create your views here.
 
 class UserRegisterView(View):
@@ -26,3 +32,37 @@ class UserRegisterView(View):
             messages.success(request, 'you registered successfully', 'success')
             return redirect('home:home')
         return render(request, self.template_name, {'form': form})
+
+
+
+
+class UserLoginView(View):
+	form_class = UserLoginForm
+	template_name = 'account/login.html'
+
+	def setup(self, request, *args, **kwargs):
+		self.next = request.GET.get('next')
+		return super().setup(request, *args, **kwargs)
+
+	def dispatch(self, request, *args, **kwargs):
+		if request.user.is_authenticated:
+			return redirect('home:home')
+		return super().dispatch(request, *args, **kwargs)
+
+	def get(self, request):
+		form = self.form_class
+		return render(request, self.template_name, {'form':form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			user = authenticate(request, username=cd['username'], password=cd['password'])
+			if user is not None:
+				login(request, user)
+				messages.success(request, 'you logged in successfully', 'success')
+				if self.next:
+					return redirect(self.next)
+				return redirect('home:home')
+			messages.error(request, 'username or password is wrong', 'warning')
+		return render(request, self.template_name, {'form':form})
